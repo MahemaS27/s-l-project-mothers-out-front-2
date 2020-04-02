d54a0119edfa3e8e472d5cde02823eccebfc6a51
Mahema Singh <mahemafire99@gmail.com>
2020-04-01 20:33:06 -0400

    reset button

--- js/viz.js
+++ js/viz.js
@@ -3,125 +3,114 @@ var width = 1000,
     centered;
 
 var tierData = [
-  {"NAME": "Total"}, 
+  {"NAME": "Total"},
   {"NAME": "Supporting"},
-  {"NAME": "Taking Action"}, 
-  {"NAME":"Leading"}];
+  {"NAME": "Taking Action"},
+  {"NAME": "Leading"}];
 
 var currentState = null; // need to figure out how to populate this?????
 
-
 // adding the filter before we draw the map
 
-
-
-
 var dropDown = d3.select("#map-container")
-                  .append("select")
-                  .attr("tier", "tier-list");
-
-console.log(dropDown);
+.append("select")
+.attr("tier", "tier-list");
 
 var options = dropDown.selectAll('option')
-      .data(tierData)
-      .enter()
-      .append("option");
-options.text(function(d){
+.data(tierData)
+.enter()
+.append("option");
+options.text(function (d) {
   return d.NAME;
 })
-.attr("value", function(d){
+.attr("value", function (d) {
   return d.NAME;
 });
 
-
-var svg = d3.
-select('#map-container')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-
-
+var svg = d3.select('#map-container')
+.append('svg')
+.attr('width', width)
+.attr('height', height)
 
 var projection = d3
-    .geoAlbersUsa()
-    .translate([width / 2, height / 2])
-    .scale(width);
-
+.geoAlbersUsa()
+.translate([width / 2, height / 2])
+.scale(width);
 
 var path = d3.geoPath().projection(projection);
 
 var state_data = {};
 
+d3.json("js/us.json", function (us) {
+  d3.tsv("data/us-state-names.tsv", function (data) {
 
-d3.json("js/us.json", function(us) {
-    d3.tsv("data/us-state-names.tsv", function(data){
+    for (i of data) {
 
-        for (i of data) {
+      state_data[i.id] = i.name
+    }
 
-            state_data[i.id] = i.name
-        }
+    //console.log(state_data);
+    drawMap(us);
 
-        //console.log(state_data);
-        drawMap(us);
+  })
 
-    })
-    
 });
-
-
 
 function drawMap(us) {
 
-    //drawing map boundaries
+  //drawing map boundaries
 
-    var mapgroup = svg.append("g").attr("class", "mapgroup")
+  var mapgroup = svg.append("g").attr("class", "mapgroup")
 
-    mapgroup.append("g")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
-        .enter()
-        .append("path")
-        .attr("d", path) // add projectiong
-        // clicked state change color to orange
-        .on("click", function (d) {
+  mapgroup.append("g")
+  .selectAll("path")
+  .data(topojson.feature(us, us.objects.states).features)
+  .enter()
+  .append("path")
+  .attr("d", path) // add projectiong
+  // clicked state change color to orange
+  .on("click", onStateClick)
+  .attr("class", "states");
 
-
-            //console.log(d);
-
-        d3.selectAll('path').style('fill',null);
-        d3.select(this).style("fill","orange");   // need to add a reset button here - make all classed object that are orange back to grey
-
-
-        // here populate current state variable
-
-        //console.log(state_data[d.id]); // id correseponds to specific state
-
-        // set current state varaible
-
-
+// adding state border
+  mapgroup
+  .append('path')
+  .datum(
+      topojson.mesh(us, us.objects.states, function (a, b) {
+        return a !== b;
       })
-        .attr("class", "states");
-
-
-// adding state border 
-    mapgroup
-        .append('path')
-        .datum(
-            topojson.mesh(us, us.objects.states, function(a, b) {
-                return a !== b;
-            })
-        )
-        .attr("id", "state-borders")
-        .attr("d", path);
-
-
+  )
+  .attr("id", "state-borders")
+  .attr("d", path);
 
 }
 
-// getter method for returning 
+// getter method for return the current state
+function getCurrentState() {
+  return currentState;
+}
 
+//on state click function
 
+function onStateClick(d){
+  d3.selectAll('path').style('fill', null);
+  d3.select(this).style("fill", "orange");   // need to add a reset button here - make all classed object that are orange back to grey
 
+  // here populate current state variable when user clicks
+
+  currentState = state_data[d.id];
+
+  console.log(currentState);
+}
+
+function reset(){
+
+  d3.selectAll('path').style('fill', '#aaa');
+  currentState = null;
+
+  console.log(currentState);
+
+}
 
 
 
